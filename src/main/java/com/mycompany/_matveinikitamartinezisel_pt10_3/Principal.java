7b5838172e5d8ae5727f257cd03a344a4ab01de9
nikita <>
2025-03-05 08:57:04 +0100

    Versió incial

---
+++ src/main/java/com/mycompany/_matveinikitamartinezisel_pt10_3/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._matveinikitamartinezisel_pt10_3;
+
+/**
+ *
+ * @author nikit
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
